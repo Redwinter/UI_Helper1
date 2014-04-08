4bc43bccf730969fa51f5f302053b8e2874938da
Erik Fleuter <red.winter@gmail.com>
2014-04-08 15:03:36 -0700

    Initial views setup
    - table views seem shit for menus. will be retrying

--- UI_Helper1/ViewController.h
+++ UI_Helper1/ViewController.h
@@ -10,4 +10,25 @@
 
 @interface ViewController : UIViewController
 
+@property (strong,nonatomic) CIContext *context;
+@property (strong, nonatomic) IBOutlet UIView *viewEditMenu;
+@property (strong, nonatomic) IBOutlet UIView *editVisMenu;
+@property (strong, nonatomic) IBOutlet UISwitch *BBar_Vis;
+@property (strong, nonatomic) IBOutlet UISwitch *TBar_Vis;
+@property (strong, nonatomic) IBOutlet UISwitch *HudL_Vis;
+@property (strong, nonatomic) IBOutlet UISwitch *HudR_Vis;
+@property (strong, nonatomic) IBOutlet UISwitch *BigLabel_Vis;
+@property (strong, nonatomic) IBOutlet UISwitch *MedLabel_Vis;
+
+
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundView;
+@property (strong,nonatomic) IBOutlet UIPickerView *filterPicker;
+@property (strong) UIPopoverController *popoverImageViewController;
+
+- (IBAction)pickImage:(id)sender;
+- (IBAction)showVisEditMenu:(id)sender;
+- (IBAction)showViewPropsMenu:(id)sender;
+- (IBAction)enableViewMoves:(id)sender;
+
+
 @end
--- UI_Helper1/ViewController.m
+++ UI_Helper1/ViewController.m
@@ -20,6 +20,37 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+
+
+
+- (IBAction)pickImage:(id)sender
+{
+    
+}
+
+- (IBAction)showVisEditMenu:(id)sender
+{
+    self.editVisMenu.hidden = !self.editVisMenu.hidden;
+}
+
+- (IBAction)showViewPropsMenu:(id)sender
+{
+    // NOTE: temp on button trigger. remove and have AFTER a view touched & moved
+    // & have button enable dragging views around and pos
+    self.viewEditMenu.hidden = !self.viewEditMenu.hidden;
+}
+
+- (IBAction)enableViewMoves:(id)sender
+{
+    
+}
+
+
+
+
+
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
